dfae7c39bf285c4f7be4cb103918ad1a6dda83cf
Nacamura <t.nacamura@gmail.com>
2014-11-16 23:31:43 +0900

    modify for clockwork

--- crawl_with_libra.rb
+++ crawl_with_libra.rb
@@ -1,7 +1,19 @@
 load 'libra_models.rb'
 
-Library.each do |l|
-  l.sendmail = true
-  l.check_new_book
+class NishitokyoCrawler
+  def call
+    Library.where(name: "西東京市").each do |l|
+      l.sendmail = true
+      l.check_new_book
+    end
+  end
 end
 
+class ComicCrawler
+  def call
+    Library.where(name: "コミック").each do |l|
+      l.sendmail = true
+      l.check_new_book
+    end
+  end
+end
